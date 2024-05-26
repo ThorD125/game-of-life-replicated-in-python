6c8858dbe293a90807e7e4d70b41178b3c99c4e1
thor <thor.demeestere@telenet.be>
2024-05-26 13:46:38 +0200

    basics

--- python/circle.py
+++ python/circle.py
@@ -1,10 +1,15 @@
 import pygame
 import math
 import random
+import threading
 
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
+speed = 2
+line_width = 5
+
+
 big_circle_center = (WIDTH // 2, HEIGHT // 2)
 big_circle_radius = 200
 
@@ -21,8 +26,23 @@ CURRENT_COLOR = BLUE
 small_circle_radius = 20
 small_circle_pos = [WIDTH // 2, HEIGHT // 2 -
                     big_circle_radius + small_circle_radius]
-speed = 2
 velocity = [speed, speed]
+
+clock = pygame.time.Clock()
+angle = math.pi / 5
+collision_point = None
+
+def play_sound(mp3_file, volume=0.5):
+    def _play():
+        pygame.mixer.init()
+        pygame.mixer.music.load(mp3_file)
+        pygame.mixer.music.set_volume(volume)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+    sound_thread = threading.Thread(target=_play)
+    sound_thread.start()
+
 def is_within_big_circle(pos, big_center, big_radius, small_radius):
     dist = math.sqrt((pos[0] - big_center[0])**2 + (pos[1] - big_center[1])**2)
     return dist + small_radius <= big_radius
@@ -39,39 +59,35 @@ def reflect_velocity(pos, vel, big_center):
 
     return new_vel
 
-running = True
-clock = pygame.time.Clock()
 def increase_speed():
     global speed
     if (speed < big_circle_radius / 2):
         speed += 0.5
 
 def random_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randin (0, 255))
-
+    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            
 def calculate_line_points(big_center, big_radius, angle):
     x = big_center[0] + big_radius * math.cos(angle)
     y = big_center[1] + big_radius * math.sin(angle)
     return (x, y)
 
-angle = math.pi / 5
 
-collision_point = None
-def draw_lines(circle_position):
-    # calc_point = calculate_line_points(
-        # big_circle_center, big_circle_radius, angle)
-        
+def draw_line(circle_position, width):
     if collision_point:
-        pygame.draw.line(screen, CURRENT_COLOR, collision_point, circle_position, 2)
+        pygame.draw.line(screen, CURRENT_COLOR, collision_point, circle_position, width)
 
 def calculate_collision_point(pos, big_center, big_radius, small_radius):
     normal = [pos[0] - big_center[0], pos[1] - big_center[1]]
     normal_magnitude = math.sqrt(normal[0]**2 + normal[1]**2)
+    
     global collision_point
     collision_point = [
         big_center[0] + (big_radius) * (normal[0] / normal_magnitude),
         big_center[1] + (big_radius) * (normal[1] / normal_magnitude)
     ]
+    
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -82,28 +98,24 @@ while running:
 
     if not is_within_big_circle(small_circle_pos, big_circle_center, big_circle_radius, small_circle_radius):
         calculate_collision_point(small_circle_pos, big_circle_center, big_circle_radius, small_circle_radius)        
-
         velocity = reflect_velocity(
             small_circle_pos, velocity, big_circle_center)
         increase_speed()
-
         vel_magnitude = math.sqrt(velocity[0]**2 + velocity[1]**2)
-
         velocity = [speed * (velocity[0] / vel_magnitude),
                     speed * (velocity[1] / vel_magnitude)]
-
         CURRENT_COLOR = random_color()
+        play_sound("sound/lq.mp3")
+
 
     screen.fill(BLACK)
 
-    draw_lines(small_circle_pos)
-
+    draw_line(small_circle_pos, line_width)
     pygame.draw.circle(screen, CURRENT_COLOR,
-                    big_circle_center, big_circle_radius, 2)
+                    big_circle_center, big_circle_radius, line_width)
 
     pygame.draw.circle(screen, WHITE, small_circle_pos, small_circle_radius)
-
     pygame.display.flip()
-
     clock.tick(60)
+    
 pygame.quit()
